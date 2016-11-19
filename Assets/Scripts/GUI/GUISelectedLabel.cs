1b22b8c6c98f4f02b22381a98e51380000fa73b5
fdj <janviernguyen@gmail.com>
2016-11-19 22:54:01 +0700

    improve selected info

--- Assets/Scripts/GUI/GUISelectedLabel.cs
+++ Assets/Scripts/GUI/GUISelectedLabel.cs
@@ -1,6 +1,8 @@
 ﻿using Entitas;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text;
+
 
 public class GUISelectedLabel : MonoBehaviour, ISelectInputListener {
 	Text mText;
@@ -12,7 +14,15 @@ public class GUISelectedLabel : MonoBehaviour, ISelectInputListener {
 
 	public void OnSelected ()
 	{
-		if (Pools.sharedInstance.input.selectInputEntity.selectInput.entity != null)
-			mText.text = Pools.sharedInstance.input.selectInputEntity.selectInput.entity.name.value;
+		if (Pools.sharedInstance.input.selectInputEntity.selectInput.entity != null) {
+			StringBuilder sb = new StringBuilder ();
+			sb.Append("Name ").Append (Pools.sharedInstance.input.selectInputEntity.selectInput.entity.name.value).Append ("\n")
+				.Append("Level ").Append (Pools.sharedInstance.input.selectInputEntity.selectInput.entity.currentLevel.value).Append ("\n")
+				.Append("Exp ").Append (Pools.sharedInstance.input.selectInputEntity.selectInput.entity.currentExp.value).Append ("/").Append (Pools.sharedInstance.input.selectInputEntity.selectInput.entity.exp.value).Append ("\n")
+				.Append("Hp ").Append (Pools.sharedInstance.input.selectInputEntity.selectInput.entity.currentHitPoint.amount).Append ("/").Append (Pools.sharedInstance.input.selectInputEntity.selectInput.entity.hitPoint.amount).Append ("\n")
+			;
+
+			mText.text = sb.ToString();
+		}
 	}
 }
